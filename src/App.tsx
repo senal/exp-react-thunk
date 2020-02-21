de88fb36154eed10eb58e95a6c0e69946305ebad
Ranga Fernando <Ranga.Fernando@efic.gov.au>
2020-02-21 14:44:48 +1100

    remove thunk from the store for the timebeing

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,12 @@ function App() {
   return (
     <Router>
     <Menu />
-      <Route exact path="/" component={Home}></Route>
-      <Route  path="/member" component={Member}></Route>
-      <Route  path="/list" component={List}></Route>
-      <Route  path="/contact" component={() => <div>Contact</div>}></Route>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route  path="/member" component={Member}></Route>
+        <Route  path="/list" component={List}></Route>
+        <Route  path="/contact" component={() => <div>Contact</div>}></Route>
+      </Switch>
     </Router>
   );
 }
--- src/feature/store.ts
+++ src/feature/store.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from './../rootReducer';
-import thunk from "redux-thunk";
 
 const store = createStore(rootReducer);
 
